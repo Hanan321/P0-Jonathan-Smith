bbe2204cd98fe345a1a718b2f0e3706180cc0f22
Jonkermaru <jonseignur@gmail.com>
2021-07-28 11:55:00 -0700

    Added Hello.sh and commandsUsed.md

---
+++ Hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Hello. Please write your top 3 favorite movies \(no spaces, please\).
+read movies1 movies2 movies3
+echo Cool! So you like $movies1, $movies2, and $movies3. Thanks for sharing.
---
+++ commandsUsed.md
@@ -0,0 +1,7 @@
+cd to the directory I wanted my repository in.
+git clone <my git url>
+cd myrepository
+touch Hello.sh
+code Hello.sh
+touch commandsUsed.md
+vi commandsUsed.md
